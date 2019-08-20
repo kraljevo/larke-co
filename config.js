2e57465a66c4d091af126dc9133fbdc1d20cc0d1
Matthew Tetrault <mktetrault@yahoo.com>
2019-08-20 08:31:30 -0400

    updated content, minor formatting

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteTitle: 'Larke & Co.', // <title>
+  manifestName: 'Prologue',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Larke & Co.',
+  heading: 'Boutique Store in Kokomo, Indiana',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://www.facebook.com/larkeco/',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Message',
+      url: 'mailto:',
+    },
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,164 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import PageFooter from '../components/PageFooter';
+import SideBar from '../components/SideBar';
+
+import pic2 from '../assets/images/pic02.jpg';
+import pic3 from '../assets/images/pic03.jpg';
+import pic4 from '../assets/images/pic04.jpg';
+import pic5 from '../assets/images/pic05.jpg';
+import pic6 from '../assets/images/pic06.jpg';
+import pic7 from '../assets/images/pic07.jpg';
+import pic8 from '../assets/images/pic08.jpg';
+import Scroll from '../components/Scroll';
+
+const sections = [
+  { id: 'top', name: 'Home', icon: 'fa-home' },
+  { id: 'portfolio', name: 'Products', icon: 'fa-th' },
+  { id: 'about', name: 'About', icon: 'fa-user' },
+  { id: 'contact', name: 'Contact', icon: 'fa-envelope' },
+];
+
+const IndexPage = () => (
+  <Layout>
+    <SideBar sections={sections} />
+
+    <div id="main">
+      <section id="top" className="one dark cover">
+        <div className="container">
+          <header>
+            <h2 className="alt">Larke & Co.</h2>
+            <p>Eclectic Goods for the Modern Soul</p>
+          </header>
+
+          <footer>
+            <Scroll type="id" element={'portfolio'}>
+              <a href="#portfolio" className="button">
+                Show me
+              </a>
+            </Scroll>
+          </footer>
+        </div>
+      </section>
+
+      <section id="portfolio" className="two">
+        <div className="container">
+          <header>
+            <h2>Products</h2>
+          </header>
+
+          <p>
+            Larke & Co is mix of vintage items plus boutique clothing & accessories
+          </p>
+
+          <div className="row">
+            <div className="col-4 col-12-mobile">
+              <article className="item">
+                <a href="/#" className="image fit">
+                  <img src={pic2} alt="" />
+                </a>
+                <header>
+                  <h3>Ipsum Feugiat</h3>
+                </header>
+              </article>
+              <article className="item">
+                <a href="/#" className="image fit">
+                  <img src={pic3} alt="" />
+                </a>
+                <header>
+                  <h3>Rhoncus Semper</h3>
+                </header>
+              </article>
+            </div>
+            <div className="col-4 col-12-mobile">
+              <article className="item">
+                <a href="/#" className="image fit">
+                  <img src={pic4} alt="" />
+                </a>
+                <header>
+                  <h3>Magna Nullam</h3>
+                </header>
+              </article>
+              <article className="item">
+                <a href="/#" className="image fit">
+                  <img src={pic5} alt="" />
+                </a>
+                <header>
+                  <h3>Natoque Vitae</h3>
+                </header>
+              </article>
+            </div>
+            <div className="col-4 col-12-mobile">
+              <article className="item">
+                <a href="/#" className="image fit">
+                  <img src={pic6} alt="" />
+                </a>
+                <header>
+                  <h3>Dolor Penatibus</h3>
+                </header>
+              </article>
+              <article className="item">
+                <a href="/#" className="image fit">
+                  <img src={pic7} alt="" />
+                </a>
+                <header>
+                  <h3>Orci Convallis</h3>
+                </header>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="about" className="three">
+        <div className="container">
+          <header>
+            <h2>About</h2>
+          </header>
+
+          <a href="/#" className="image featured">
+            <img src={pic8} alt="" />
+          </a>
+
+          <p>
+            We are locally-owned and operated in Kokomo, Indiana. We believe every item has a story to tell, and we want to help you hear it.
+          </p>
+        </div>
+      </section>
+
+      <section id="contact" className="four">
+        <div className="container">
+          <header>
+            <h2>Contact</h2>
+          </header>
+
+          <p>
+            Don't know where to start? Have questions? Send me a message!
+          </p>
+
+          <form method="post" action="#">
+            <div className="row">
+              <div className="col-6 col-12-mobile">
+                <input type="text" name="name" placeholder="Name" />
+              </div>
+              <div className="col-6 col-12-mobile">
+                <input type="text" name="email" placeholder="Email" />
+              </div>
+              <div className="col-12">
+                <textarea name="message" placeholder="Message" />
+              </div>
+              <div className="col-12">
+                <input type="submit" value="Send Message" />
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+
+    <PageFooter />
+  </Layout>
+);
+
+export default IndexPage;
